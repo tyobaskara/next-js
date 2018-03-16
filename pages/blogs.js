1e382013788fc11aa54c23b512e4d722df93fbfa
Prasetya Aji <prasetya.aji@reservasi.com>
2018-03-16 15:16:43 +0700

    styling jsx

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,5 +1,6 @@
 import Layout from '../lib/Layout'
 import Link from 'next/link'
+import Markdown from 'react-markdown'
 
 const Blog = (props) => (
   <li>
@@ -12,10 +13,42 @@ const Blog = (props) => (
 export default () => (
   <Layout>
     <h1>My Blog</h1>
+    <div className="markdown">
+     <Markdown source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
+
+### This is a title
+
+And here's the content.
+     `}/>
+   </div>
     <ul>
       <Blog title="Hello Next.js"/>
       <Blog title="Learn Next.js is awesome"/>
       <Blog title="Deploy apps with Zeit"/>
     </ul>
+    
+    <style jsx global>{`
+      .markdown {
+        font-family: 'Arial';
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+  `}</style>
   </Layout>
 )
